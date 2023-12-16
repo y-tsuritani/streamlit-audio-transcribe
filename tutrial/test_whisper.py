4f094dd0265d061dd9a97372eab317b497154c61
y-tsuritani <ykt.tsuriya@gmail.com>
2023-12-17 07:26:07 +0900

    modify: チュートリアルファイルの変更

--- tutrial/test_whisper.py
+++ tutrial/test_whisper.py
@@ -10,9 +10,9 @@ api_key = os.getenv("OPENAI_API_KEY")
 
 client = OpenAI(api_key=api_key)
 
-audio_file = open("../audio/rusuden_04.wav", "rb")
-transcript = client.audio.transcriptions.create(
-    model="whisper-1", file=audio_file, response_format="text"
-)
+with open("../audio/test.mp3", "rb") as audio_file:
+    transcript = client.audio.transcriptions.create(
+        model="whisper-1", file=audio_file, response_format="text"
+    )
 
 print(transcript)
